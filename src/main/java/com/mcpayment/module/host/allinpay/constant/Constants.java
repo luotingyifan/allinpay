aacbe1f3d6b14c2d9cde2155eff27a721a309760
gongjing <gongjing@mcpayment.net>
2017-08-02 15:59:34 +0800

    sss

---
+++ src/main/java/com/mcpayment/module/host/allinpay/constant/Constants.java
@@ -0,0 +1,44 @@
+package com.mcpayment.module.host.allinpay.constant;
+
+public class Constants {
+
+    // Alipay, partnerId and appId gateway
+    public static final String API_URL = "https://mapi.alipay.com/gateway.do";
+
+    public static final String REQUEST_METHOD = "POST";
+
+    public static final String CHARSET = "UTF-8";
+
+    public static final String SIGN = "sign";
+
+    public static final String SIGN_TYPE = "sign_type";
+
+    public static final String SIGN_TYPE_CODE = "MD5";
+
+    public static final String TRADE_PAY = "alipay.acquire.overseas.spot.pay";
+
+    public static final String TRADE_PAY_QUERY = "alipay.acquire.overseas.query";
+
+    public static final String TRADE_CANCEL = "alipay.acquire.cancel";
+
+    public static final String TRADE_REFUND = "alipay.acquire.overseas.spot.refund";
+
+    public static final String TRADE_PRECREATE = "alipay.acquire.precreate";
+
+    public static final String TRADE_RATE_QUERY = "alipay.acquire.rate.query";
+
+    public static final String FEE_TYPE_CNY = "CNY";
+
+    public static final String IS_SYNC = "N";
+
+    public static final String PRODUCT_CODE = "OVERSEAS_MBARCODE_PAY";
+
+    public static final String IDENTITY_CODE_TYPE_BARCODE = "barcode";
+
+    public static final String ALIPAY_KEY = "ALIPAY_KEY";
+
+    public static final String ALIPAY_MERCHANT_ID = "ALIPAY_MERCHANT_ID";
+
+    public static final String ALIPAY_MPOS_NOTIFY_URL = "ALIPAY_MPOS_NOTIFY_URL";
+
+}
---
+++ src/main/java/com/mcpayment/module/host/allinpay/service/AllinpayHostModule.java
@@ -0,0 +1,19 @@
+package com.mcpayment.module.host.allinpay.service;
+
+import com.mcpayment.module.model.transaction.WalletMessage;
+
+public interface AllinpayHostModule {
+
+//    WalletMessage tradePay(WalletMessage message) throws Exception;
+//
+//    WalletMessage tradeQuery(WalletMessage message) throws Exception;
+//
+//    WalletMessage tradeCancel(WalletMessage message) throws Exception;
+//
+//    WalletMessage tradeRefund(WalletMessage message) throws Exception;
+//
+//    WalletMessage tradePrecreate(WalletMessage message) throws Exception;
+//
+//    WalletMessage rateQuery(WalletMessage message) throws Exception;
+
+}
