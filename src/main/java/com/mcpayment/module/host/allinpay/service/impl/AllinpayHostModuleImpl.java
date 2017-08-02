0e5f111e1d7323349d274c991c9a453a35333fc6
luoting <luoting@mcpayment.net>
2017-08-02 17:11:14 +0800

    Add : Test correlation

---
+++ src/main/java/com/mcpayment/module/host/allinpay/service/impl/AllinpayHostModuleImpl.java
@@ -0,0 +1,9 @@
+package com.mcpayment.module.host.allinpay.service.impl;
+
+import com.mcpayment.module.host.allinpay.service.AllinpayHostModule;
+
+/**
+ * Created by luoting on 2017/8/2.
+ */
+public class AllinpayHostModuleImpl implements AllinpayHostModule {
+}
